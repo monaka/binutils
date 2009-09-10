c6a46cf76f37d8e68292f675eacd4033370b2045
nickc <nickc>
2009-09-10 13:42:07 +0000

    Update soruces to compile cleanly with -Wc++-compat:
            * addr2line.c (slurp_symtab): Fix casts. Introduce variable
            minisyms to avoid aliasing varning.
            * ar.c: Add casts.
            (normalize): Use name del instead of delete.
            (display_target_list,display_info_table): Change loop counter
            variable a to int.
            * bucomm.c: Add casts.
            * debug.c: Update function to use new names.
            (struct debug_baseclass): Rename member from virtual to
            is_virtual.
            (struct debug_type_s, struct debug_field_s, struct
            debug_baseclass_s, struct debug_method_s, struct
            debug_method_variant_s, struct debug_type_s): Rename struct from
            avoid name collision.
            * debug.h: Use new struct names.
            * dwarf.c: Add casts.
            (free_debug_memory): Change loop counter variable a to int.
            * ieee.c: Add casts.
            (enum ieee_var_kind): Move to top level.
            (ieee_class_baseclass): Rename parameter virtual to is_virtual.
            (ieee_class_method_var): Rename variable virtual to is_virtual.
            * nm.c: Add casts.
            * objcopy.c: Add casts.
            (copy_archive): Rename variable delete to del.
            * objdump.c: Add casts.
            (dump_dwarf_section): Change loop counter variable i to int.
            * prdbg.c: Add casts.
            (pr_class_baseclass,tg_class_baseclass): Rename parameters virtual
            to is_virtual.
            * readelf.c: Add casts.
            (struct ia64_unw_table_entry,struct hppa_unw_table_entry): Move to
            top level.
            * size.c: Add casts.
            * stabs.c (parse_stab_type, parse_stab_range_type,
            (parse_stab_cpp_abbrev): Rename parameter from typename to
            type_name.
            (parse_stab_baseclasses): Rename variable virtual to is_virtual.
            * strings.c: Add casts.
            * wrstabs.c (stab_class_baseclass): Rename parameter virtual to
            is_virtual.

--- binutils/addr2line.c
+++ binutils/addr2line.c
@@ -1,6 +1,6 @@
 /* addr2line.c -- convert addresses to line number and function name
-   Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-   2007, 2009  Free Software Foundation, Inc.
+   Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+   Free Software Foundation, Inc.
    Contributed by Ulrich Lauther <Ulrich.Lauther@mchp.siemens.de>
 
    This file is part of GNU Binutils.
@@ -37,13 +37,10 @@
 #include "libiberty.h"
 #include "demangle.h"
 #include "bucomm.h"
-#include "elf-bfd.h"
 
 static bfd_boolean unwind_inlines;	/* -i, unwind inlined functions. */
-static bfd_boolean with_addresses;	/* -a, show addresses.  */
 static bfd_boolean with_functions;	/* -f, show function names.  */
 static bfd_boolean do_demangle;		/* -C, demangle names.  */
-static bfd_boolean pretty_print;	/* -p, print on one line.  */
 static bfd_boolean base_names;		/* -s, strip directory names.  */
 
 static int naddr;		/* Number of addresses to process.  */
@@ -53,13 +50,11 @@ static asymbol **syms;		/* Symbol table.  */
 
 static struct option long_options[] =
 {
-  {"addresses", no_argument, NULL, 'a'},
   {"basenames", no_argument, NULL, 's'},
   {"demangle", optional_argument, NULL, 'C'},
   {"exe", required_argument, NULL, 'e'},
   {"functions", no_argument, NULL, 'f'},
   {"inlines", no_argument, NULL, 'i'},
-  {"pretty-print", no_argument, NULL, 'p'},
   {"section", required_argument, NULL, 'j'},
   {"target", required_argument, NULL, 'b'},
   {"help", no_argument, NULL, 'H'},
@@ -83,12 +78,10 @@ usage (FILE *stream, int status)
   fprintf (stream, _(" If no addresses are specified on the command line, they will be read from stdin\n"));
   fprintf (stream, _(" The options are:\n\
   @<file>                Read options from <file>\n\
-  -a --addresses         Show addresses\n\
   -b --target=<bfdname>  Set the binary file format\n\
   -e --exe=<executable>  Set the input file name (default is a.out)\n\
   -i --inlines           Unwind inlined functions\n\
   -j --section=<name>    Read section-relative offsets instead of addresses\n\
-  -p --pretty-print      Make the output easier to read for humans\n\
   -s --basenames         Strip directory names\n\
   -f --functions         Show function names\n\
   -C --demangle[=style]  Demangle function names\n\
@@ -107,27 +100,17 @@ usage (FILE *stream, int status)
 static void
 slurp_symtab (bfd *abfd)
 {
-  long storage;
   long symcount;
-  bfd_boolean dynamic = FALSE;
+  unsigned int size;
+  void *minisyms = &syms;
 
   if ((bfd_get_file_flags (abfd) & HAS_SYMS) == 0)
     return;
 
-  storage = bfd_get_symtab_upper_bound (abfd);
-  if (storage == 0)
-    {
-      storage = bfd_get_dynamic_symtab_upper_bound (abfd);
-      dynamic = TRUE;
-    }
-  if (storage < 0)
-    bfd_fatal (bfd_get_filename (abfd));
+  symcount = bfd_read_minisymbols (abfd, FALSE, &minisyms, &size);
+  if (symcount == 0)
+    symcount = bfd_read_minisymbols (abfd, TRUE /* dynamic */, &minisyms, &size);
 
-  syms = (asymbol **) xmalloc (storage);
-  if (dynamic)
-    symcount = bfd_canonicalize_dynamic_symtab (abfd, syms);
-  else
-    symcount = bfd_canonicalize_symtab (abfd, syms);
   if (symcount < 0)
     bfd_fatal (bfd_get_filename (abfd));
 }
@@ -196,8 +179,6 @@ find_offset_in_section (bfd *abfd, asection *section)
 static void
 translate_addresses (bfd *abfd, asection *section)
 {
-  const struct elf_backend_data * bed;
-
   int read_stdin = (naddr == 0);
 
   for (;;)
@@ -218,23 +199,6 @@ translate_addresses (bfd *abfd, asection *section)
 	  pc = bfd_scan_vma (*addr++, NULL, 16);
 	}
 
-      if (bfd_get_flavour (abfd) == bfd_target_elf_flavour
-	  && (bed = get_elf_backend_data (abfd)) != NULL
-	  && bed->sign_extend_vma
-	  && (pc & (bfd_vma) 1 << (bed->s->arch_size - 1)))
-	pc |= ((bfd_vma) -1) << bed->s->arch_size;
-
-      if (with_addresses)
-        {
-          printf ("0x");
-          bfd_printf_vma (abfd, pc);
-
-          if (pretty_print)
-            printf (": ");
-          else
-            printf ("\n");
-        }
-
       found = FALSE;
       if (section)
 	find_offset_in_section (abfd, section);
@@ -249,64 +213,44 @@ translate_addresses (bfd *abfd, asection *section)
 	}
       else
 	{
-	  while (1)
-            {
-              if (with_functions)
-                {
-                  const char *name;
-                  char *alloc = NULL;
+	  do {
+	    if (with_functions)
+	      {
+		const char *name;
+		char *alloc = NULL;
 
-                  name = functionname;
-                  if (name == NULL || *name == '\0')
-                    name = "??";
-                  else if (do_demangle)
-                    {
-                      alloc = bfd_demangle (abfd, name, DMGL_ANSI | DMGL_PARAMS);
-                      if (alloc != NULL)
-                        name = alloc;
-                    }
+		name = functionname;
+		if (name == NULL || *name == '\0')
+		  name = "??";
+		else if (do_demangle)
+		  {
+		    alloc = bfd_demangle (abfd, name, DMGL_ANSI | DMGL_PARAMS);
+		    if (alloc != NULL)
+		      name = alloc;
+		  }
 
-                  printf ("%s", name);
-                  if (pretty_print)
-		    /* Note for translators:  This printf is used to join the
-		       function name just printed above to the line number/
-		       file name pair that is about to be printed below.  Eg:
+		printf ("%s\n", name);
 
-		         foo at 123:bar.c  */
-                    printf (_(" at "));
-                  else
-                    printf ("\n");
+		if (alloc != NULL)
+		  free (alloc);
+	      }
 
-                  if (alloc != NULL)
-                    free (alloc);
-                }
+	    if (base_names && filename != NULL)
+	      {
+		char *h;
 
-              if (base_names && filename != NULL)
-                {
-                  char *h;
+		h = strrchr (filename, '/');
+		if (h != NULL)
+		  filename = h + 1;
+	      }
 
-                  h = strrchr (filename, '/');
-                  if (h != NULL)
-                    filename = h + 1;
-                }
+	    printf ("%s:%u\n", filename ? filename : "??", line);
+	    if (!unwind_inlines)
+	      found = FALSE;
+	    else
+	      found = bfd_find_inliner_info (abfd, &filename, &functionname, &line);
+	  } while (found);
 
-              printf ("%s:%u\n", filename ? filename : "??", line);
-              if (!unwind_inlines)
-                found = FALSE;
-              else
-                found = bfd_find_inliner_info (abfd, &filename, &functionname,
-					       &line);
-              if (! found)
-                break;
-              if (pretty_print)
-		/* Note for translators: This printf is used to join the
-		   line number/file name pair that has just been printed with
-		   the line number/file name pair that is going to be printed
-		   by the next iteration of the while loop.  Eg:
-
-		     123:bar.c (inlined by) 456:main.c  */
-                printf (_(" (inlined by) "));
-            }
 	}
 
       /* fflush() is essential for using this command as a server
@@ -333,9 +277,6 @@ process_file (const char *file_name, const char *section_name,
   abfd = bfd_openr (file_name, target);
   if (abfd == NULL)
     bfd_fatal (file_name);
-
-  /* Decompress sections.  */
-  abfd->flags |= BFD_DECOMPRESS;
 
   if (bfd_check_format (abfd, bfd_archive))
     fatal (_("%s: cannot get addresses from archive"), file_name);
@@ -403,16 +344,13 @@ main (int argc, char **argv)
   file_name = NULL;
   section_name = NULL;
   target = NULL;
-  while ((c = getopt_long (argc, argv, "ab:Ce:sfHhij:pVv", long_options, (int *) 0))
+  while ((c = getopt_long (argc, argv, "b:Ce:sfHhij:Vv", long_options, (int *) 0))
 	 != EOF)
     {
       switch (c)
 	{
 	case 0:
 	  break;		/* We've been given a long option.  */
-	case 'a':
-	  with_addresses = TRUE;
-	  break;
 	case 'b':
 	  target = optarg;
 	  break;
@@ -439,9 +377,6 @@ main (int argc, char **argv)
 	case 'f':
 	  with_functions = TRUE;
 	  break;
-        case 'p':
-          pretty_print = TRUE;
-          break;
 	case 'v':
 	case 'V':
 	  print_version ("addr2line");
--- binutils/strings.c
+++ binutils/strings.c
@@ -1,6 +1,6 @@
 /* strings -- print the strings of printable characters in files
    Copyright 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
-   2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2011
+   2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
@@ -80,6 +80,21 @@ extern int errno;
 /* The BFD section flags that identify an initialized data section.  */
 #define DATA_FLAGS (SEC_ALLOC | SEC_LOAD | SEC_HAS_CONTENTS)
 
+#ifdef HAVE_FOPEN64
+typedef off64_t file_off;
+#define file_open(s,m) fopen64(s, m)
+#else
+typedef off_t file_off;
+#define file_open(s,m) fopen(s, m)
+#endif
+#ifdef HAVE_STAT64
+typedef struct stat64 statbuf;
+#define file_stat(f,s) stat64(f, s)
+#else
+typedef struct stat statbuf;
+#define file_stat(f,s) stat(f, s)
+#endif
+
 /* Radix for printing addresses (must be 8, 10 or 16).  */
 static int address_radix;
 
@@ -130,9 +145,9 @@ typedef struct
 static void strings_a_section (bfd *, asection *, void *);
 static bfd_boolean strings_object_file (const char *);
 static bfd_boolean strings_file (char *file);
-static void print_strings (const char *, FILE *, file_ptr, int, int, char *);
+static void print_strings (const char *, FILE *, file_off, int, int, char *);
 static void usage (FILE *, int);
-static long get_char (FILE *, file_ptr *, int *, char **);
+static long get_char (FILE *, file_off *, int *, char **);
 
 int main (int, char **);
 
@@ -399,11 +414,9 @@ strings_object_file (const char *file)
 static bfd_boolean
 strings_file (char *file)
 {
-  struct stat st;
+  statbuf st;
 
-  /* get_file_size does not support non-S_ISREG files.  */
-
-  if (stat (file, &st) < 0)
+  if (file_stat (file, &st) < 0)
     {
       if (errno == ENOENT)
 	non_fatal (_("'%s': No such file"), file);
@@ -421,7 +434,7 @@ strings_file (char *file)
     {
       FILE *stream;
 
-      stream = fopen (file, FOPEN_RB);
+      stream = file_open (file, FOPEN_RB);
       if (stream == NULL)
 	{
 	  fprintf (stderr, "%s: ", program_name);
@@ -429,7 +442,7 @@ strings_file (char *file)
 	  return FALSE;
 	}
 
-      print_strings (file, stream, (file_ptr) 0, 0, 0, (char *) 0);
+      print_strings (file, stream, (file_off) 0, 0, 0, (char *) 0);
 
       if (fclose (stream) == EOF)
 	{
@@ -453,7 +466,7 @@ strings_file (char *file)
    MAGICCOUNT is how many characters are in it.  */
 
 static long
-get_char (FILE *stream, file_ptr *address, int *magiccount, char **magic)
+get_char (FILE *stream, file_off *address, int *magiccount, char **magic)
 {
   int c, i;
   long r = EOF;
@@ -529,14 +542,14 @@ get_char (FILE *stream, file_ptr *address, int *magiccount, char **magic)
    Those characters come at address ADDRESS and the data in STREAM follow.  */
 
 static void
-print_strings (const char *filename, FILE *stream, file_ptr address,
+print_strings (const char *filename, FILE *stream, file_off address,
 	       int stop_point, int magiccount, char *magic)
 {
   char *buf = (char *) xmalloc (sizeof (char) * (string_min + 1));
 
   while (1)
     {
-      file_ptr start;
+      file_off start;
       int i;
       long c;
 
@@ -549,10 +562,7 @@ print_strings (const char *filename, FILE *stream, file_ptr address,
 	{
 	  c = get_char (stream, &address, &magiccount, &magic);
 	  if (c == EOF)
-	    {
-	      free (buf);
-	      return;
-	    }
+	    return;
 	  if (! STRING_ISGRAPHIC (c))
 	    /* Found a non-graphic.  Try again starting with next char.  */
 	    goto tryline;
@@ -641,7 +651,6 @@ print_strings (const char *filename, FILE *stream, file_ptr address,
 
       putchar ('\n');
     }
-  free (buf);
 }
 
 static void
