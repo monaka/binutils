0dd17b5b8d5fda37dd39eb21bb07f39579143eba
muller <muller>
2011-04-21 21:16:11 +0000

    * gdb_wchar.h (USE_INTERMEDIATE_ENCODING_FUNCTION): New macro. 	(INTERMEDIATE_ENCODING): Change value to intermediate_encoding 	function call if __STDC_ISO_10646__ macro is defined. 	(intermediate_encoding): New prototype. 	* charset.c (your_gdb_wchar_t_is_bogus): New extern test variable 	to generate compile time error for unsupported gdb_wchar_t size. 	(ENDIAN_SUFFIX): New macro. 	(intermediate_encoding): New function.

--- gdb/charset.c
+++ gdb/charset.c
@@ -1,6 +1,7 @@
 /* Character set conversion support for GDB.
 
-   Copyright (C) 2001, 2003, 2007-2012 Free Software Foundation, Inc.
+   Copyright (C) 2001, 2003, 2007, 2008, 2009, 2010, 2011
+   Free Software Foundation, Inc.
 
    This file is part of GDB.
 
@@ -798,9 +799,7 @@ find_charset_names (void)
   char *args[3];
   int err, status;
   int fail = 1;
-  int flags;
   struct gdb_environ *iconv_env;
-  char *iconv_program;
 
   /* Older iconvs, e.g. 2.2.2, don't omit the intro text if stdout is
      not a tty.  We need to recognize it and ignore it.  This text is
@@ -812,26 +811,12 @@ find_charset_names (void)
 
   child = pex_init (PEX_USE_PIPES, "iconv", NULL);
 
-#ifdef ICONV_BIN
-  {
-    char *iconv_dir = relocate_gdb_directory (ICONV_BIN,
-					      ICONV_BIN_RELOCATABLE);
-    iconv_program = concat (iconv_dir, SLASH_STRING, "iconv", NULL);
-    xfree (iconv_dir);
-  }
-#else
-  iconv_program = xstrdup ("iconv");
-#endif
-  args[0] = iconv_program;
+  args[0] = "iconv";
   args[1] = "-l";
   args[2] = NULL;
-  flags = PEX_STDERR_TO_STDOUT;
-#ifndef ICONV_BIN
-  flags |= PEX_SEARCH;
-#endif
   /* Note that we simply ignore errors here.  */
-  if (!pex_run_in_environment (child, flags,
-			       args[0], args, environ_vector (iconv_env),
+  if (!pex_run_in_environment (child, PEX_SEARCH | PEX_STDERR_TO_STDOUT,
+			       "iconv", args, environ_vector (iconv_env),
 			       NULL, NULL, &err))
     {
       FILE *in = pex_read_output (child, 0);
@@ -903,7 +888,6 @@ find_charset_names (void)
 
     }
 
-  xfree (iconv_program);
   pex_free (child);
   free_environ (iconv_env);
 
