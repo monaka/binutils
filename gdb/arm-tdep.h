7aeade66b6a8d4f0d32a355c7b2d880e80fe609b
brobecke <brobecke>
2011-05-17 21:23:23 +0000

    gdb/ 	* arm-tdep.h (arm_insert_single_step_breakpoint): Add prototype. 	* arm-tdep.c (arm_override_mode): New global. 	(arm_pc_is_thumb): Respect arm_override_mode.  Remove single-step 	execution mode heuristics. 	(thumb_get_next_pc_raw): Remove INSERT_BKTP argument; always insert 	second single-step breakpoint if needed, using 	arm_insert_single_step_breakpoint. 	(arm_get_next_pc_raw): Remove INSERT_BKTP argument.  Only handle 	ARM execution mode, do not call thumb_get_next_pc_raw. 	(arm_get_next_pc): Encode execution mode in return value.  Call 	either arm_get_next_pc_raw or thumb_get_next_pc_raw. 	(arm_insert_single_step_breakpoint): New function. 	(arm_software_single_step): Call it. 	* arm-linux-tdep.c (arm_linux_sigreturn_return_addr): Add IS_THUMB 	argument to return execution mode of sigreturn target. 	(arm_linux_syscall_next_pc): Use it. 	(arm_linux_copy_svc): Update call. 	(arm_linux_software_single_step): Call 	arm_insert_single_step_breakpoint.
    gdb/testsuite/
    	* gdb.arch/thumb-singlestep.S: New file.
    	* gdb.arch/thumb-singlestep.exp: Likewise.

--- gdb/arm-tdep.h
+++ gdb/arm-tdep.h
@@ -1,5 +1,6 @@
 /* Common target dependent code for GDB on ARM systems.
-   Copyright (C) 2002-2003, 2007-2012 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2007, 2008, 2009, 2010, 2011
+   Free Software Foundation, Inc.
 
    This file is part of GDB.
 
@@ -22,7 +23,6 @@
 /* Forward declarations.  */
 struct gdbarch;
 struct regset;
-struct address_space;
 
 /* Register numbers of various important registers.  */
 
@@ -190,7 +190,7 @@ struct gdbarch_tdep
   enum struct_return struct_return;
 
   /* Cached core file helpers.  */
-  struct regset *gregset, *fpregset, *vfpregset;
+  struct regset *gregset, *fpregset;
 
   /* ISA-specific data types.  */
   struct type *arm_ext_type;
@@ -257,7 +257,8 @@ struct displaced_step_closure
     {
       /* If non-NULL, override generic SVC handling (e.g. for a particular
          OS).  */
-      int (*copy_svc_os) (struct gdbarch *gdbarch, struct regcache *regs,
+      int (*copy_svc_os) (struct gdbarch *gdbarch, uint32_t insn, CORE_ADDR to,
+			  struct regcache *regs,
 			  struct displaced_step_closure *dsc);
     } svc;
   } u;
@@ -312,7 +313,6 @@ CORE_ADDR arm_skip_stub (struct frame_info *, CORE_ADDR);
 CORE_ADDR arm_get_next_pc (struct frame_info *, CORE_ADDR);
 void arm_insert_single_step_breakpoint (struct gdbarch *,
 					struct address_space *, CORE_ADDR);
-int arm_deal_with_atomic_sequence (struct frame_info *);
 int arm_software_single_step (struct frame_info *);
 int arm_frame_is_thumb (struct frame_info *frame);
 
@@ -338,12 +338,5 @@ extern int arm_pc_is_thumb (struct gdbarch *, CORE_ADDR);
 extern const struct regset *
   armbsd_regset_from_core_section (struct gdbarch *gdbarch,
 				   const char *sect_name, size_t sect_size);
-
-/* Target descriptions.  */
-extern struct target_desc *tdesc_arm_with_m;
-extern struct target_desc *tdesc_arm_with_iwmmxt;
-extern struct target_desc *tdesc_arm_with_vfpv2;
-extern struct target_desc *tdesc_arm_with_vfpv3;
-extern struct target_desc *tdesc_arm_with_neon;
 
 #endif /* arm-tdep.h */
